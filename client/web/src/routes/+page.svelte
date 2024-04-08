c74708dd7a5d03dfa6e70e1272f696401e951570
Rafael Lima <rafael.lima@clevertech.biz>
2024-04-08 19:51:40 -0400

    feat: add initial code for the web app

---
+++ client/web/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import 'video.js/dist/video-js.css';
+	import videoJS from 'video.js';
+
+	let scrollbox: Element;
+	$: if (scrollbox) {
+		const player = videoJS(scrollbox, {
+			controls: true,
+			autoplay: true,
+			aspectRatio: '16:9',
+			html5: {
+				nativeTextTracks: false,
+				nativeAudioTracks: false,
+				vhs: {
+					overrideNative: true
+				}
+			},
+			sources: [
+				{
+					src: 'http://localhost:3000/api/stream/video.mkv',
+					type: 'application/x-mpegURL'
+				}
+			]
+		});
+	}
+</script>
+
+<video bind:this={scrollbox} controls={true} class="vjs-matrix video-js"> </video>
+<div id="audioTrackControl"></div>
